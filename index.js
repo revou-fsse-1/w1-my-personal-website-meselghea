c8c199e8c86ee5798f19b8fb497c6a4b9a88659a
meselghea <122159476+meselghea@users.noreply.github.com>
2023-01-19 21:06:18 +0700

    update 60%

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-console.log("Hello!");
+const MenuToggle = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
 
-function sayHello() {
-  alert("Hello!");
-}
+menuToggle.addEventListener('click', fuction() {
+  nav.classlist.toggle('slide');
+});
+
