f9d50a46a6ce6b085f637b42e37b89d560c7346b
meselghea <122159476+meselghea@users.noreply.github.com>
2023-01-21 12:25:46 +0700

    add alert

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
-
+console.log("Great to E-meet you!");
 
 
